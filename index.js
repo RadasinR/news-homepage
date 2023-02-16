d062b80fba9b54962e4027c7402cb8fec54d8e9c
Igor <87859179+RadasinR@users.noreply.github.com>
2023-02-16 11:01:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    let navList = document.getElementById("navbar_list");
+    navList.classList.add("show");
+}
+
+function closeMenu() {
+    let navList = document.getElementById("navbar_list");
+    navList.classList.remove("show");
+}
